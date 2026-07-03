d469bd9d8e336fd401413c45a728f416712f08f3
s0y0ungk <soyo2039@gmail.com>
2026-07-03 15:13:12 +0900

    Add view endpoint to fetch board details and improve code formatting

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const mysql = require("mysql2");
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //json->object
+app.use(express.json()); //json->object
+app.use(express.urlencoded({ extended: true })); //html form ->object
 
 let corsOptions = {
   origin: "*",
@@ -25,10 +25,23 @@ db.connect();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.get("/list", (req, res) => {
   const sqlQuery =
     "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
   db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.get("/view", (req, res) => {
+  console.log(req.query.id);
+  const id = req.query.id;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
+  const sqlQuery =
+    "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
+  db.query(sqlQuery, [id], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
